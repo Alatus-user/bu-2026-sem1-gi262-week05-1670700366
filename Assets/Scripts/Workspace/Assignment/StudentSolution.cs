06b1652934fba7141e669c58a35afd34c6825495
Alatus-user <thirasak26679@gmail.com>
2026-09-15 14:36:00 +0700

    Assignment_week05

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -1,8 +1,9 @@
-using UnityEngine;
-using System.Reflection;
-using System.Collections.Generic;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace Assignment
 {
@@ -14,7 +15,7 @@ namespace Assignment
             for (int i = 0; i< numbers.Length; i++)
             {
                 int minIndex = i;
-                for(int j = i++; j<numbers.Length; j++)
+                for(int j = i; j<numbers.Length; j++)
                 {
                     if(numbers[j] < numbers[minIndex])
                     {
@@ -89,16 +90,73 @@ namespace Assignment
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            for(int i =0; i < numbers.Length-1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                    }
+                }
+            }
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int cuurentElement = numbers[i];
+                int j = i - 1;
+
+                while (j >= 0 && numbers[j] < cuurentElement)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+
+                numbers[j + 1] = cuurentElement;
+            }
             return numbers;
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
         {
+            int n = numbers.Length;
+
+            
+            for (int i = 0; i < n - 1; i++)
+            {
+                bool swapped = false;
+
+                
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+
+                        
+
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+            int largest = numbers[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (numbers[i] < largest)
+                {
+                    return numbers[i];
+                }
+            }
+
             return 0;
         }
 
